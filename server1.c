d5dd058cac0866577e757589738aaa6be66d35a6
ruining-l <66549470+ruining-l@users.noreply.github.com>
2024-11-06 16:48:33 +0800

    Update server1.c

--- server1.c
+++ server1.c
@@ -2,7 +2,10 @@
 #include <openssl/rand.h>
 #include <string.h>
 #include <stdio.h>
-
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 // AES-XTS 加密函数
 int aes_xts_encrypt(const unsigned char *key1, const unsigned char *plaintext, size_t plaintext_len, unsigned char *TEE_SBUFFER) {
     EVP_CIPHER_CTX *ctx;
@@ -194,7 +197,47 @@ int server_main() {
     // AES-GCM 加密
     aes_gcm_encrypt(key2, GEN_SBUFFER, sizeof(GEN_SBUFFER), GEN_BUFFER, auth_tag);
 
-    // 这里应该有代码将 GEN_BUFFER 和 auth_tag 发送给客户端
+    // 将 GEN_BUFFER 和 auth_tag 发送给客户端
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    const char *hello = "Hello from server";
+
+    // 创建套接字文件描述符
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // 将套接字绑定到端口8080
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    // 发送GEN_BUFFER和auth_tag
+    send(new_socket, GEN_BUFFER, sizeof(GEN_BUFFER), 0);
+    send(new_socket, auth_tag, sizeof(auth_tag), 0);
+
+    // 关闭套接字
+    close(new_socket);
+    close(server_fd);
 
     // 清理
     memset(key1, 0, sizeof(key1));
@@ -219,7 +262,40 @@ int client_main() {
     RAND_bytes(key2, sizeof(key2));
     RAND_bytes(key3, sizeof(key3));
 
-    // 这里应该有代码从服务器接收 GEN_BUFFER 和 auth_tag
+    // 从服务器接收 GEN_BUFFER 和 auth_tag
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    // 将IPv4和IPv6地址从文本转换为二进制形式
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // 接收GEN_BUFFER和auth_tag
+    recv(sock, buffer, sizeof(GEN_BUFFER), 0);
+    memcpy(GEN_BUFFER, buffer, sizeof(GEN_BUFFER));
+    recv(sock, buffer, sizeof(auth_tag), 0);
+    memcpy(auth_tag, buffer, sizeof(auth_tag));
+
+    // ...之前的AES-GCM解密代码...
+
+    // 关闭套接字
+    close(sock);
 
     // AES-GCM 解密
     aes_gcm_decrypt(key2, GEN_BUFFER, sizeof(GEN_BUFFER), auth_tag, GEN_CBUFFER);
